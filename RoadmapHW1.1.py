60c19c8603c432189cb427201bebaa70217485ad
GizemDikenovaa <87436868+GizemDikenovaa@users.noreply.github.com>
2021-10-08 11:10:25 +0300

    FirstHomework1

--- RoadmapHW1.1.py
+++ RoadmapHW1.1.py
@@ -1,12 +1,14 @@
 def tam_bolunenler(min, max, bolen):
     sayac = 0
-    for i in range(min, max + 1):
-        if (i % bolen == 0):
+    for i in range(min, max):
+        if i % bolen == 0:
             sayac += 1
-    return (sayac)
+
+    return sayac
 
 
-min = int(input("Minimum pozitif sayi:"))
-max = int(input("Maximum pozitif sayi:"))
-bolenSayi = int(input("Bölen sayi:"))
-print("Toplamda {} sayı bölünür.".format(tam_bolunenler(min, max, bolenSayi)))
+min_sayi = int(input("minimum pozitif sayı:"))
+max_sayi = int(input("maximum pozitif sayı:"))
+bolen_sayi = int(input("bölünecek sayı:"))
+
+print("Toplam {} sayı bölünmektedir.".format(tam_bolunenler(min_sayi, max_sayi, bolen_sayi)))
--- RoadmapHW1.2.py
+++ RoadmapHW1.2.py
@@ -2,22 +2,23 @@ birler = ["", "Bir", "İki", "Üç", "Dört", "Beş", "Altı", "Yedi", "Sekiz", 
 onlar = ["", "On", "Yirmi", "Otuz", "Kırk", "Elli", "Altmış", "Yetmiş", "Seksen", "Doksan"]
 
 
-def okunus(sayiOkuma):
-    birlerBasamagi = sayi % 10
-    onlarBasamagi = sayi // 10
-    return onlar[onlarBasamagi] + " " + birler[birlerBasamagi]
+def okunus():
+    birler_basamagi = sayi % 10
+    onlar_basamagi = sayi // 10
+
+    return onlar[onlar_basamagi] + " " + birler[birler_basamagi]
 
 
-def sayiAtama(sayi):
+def sayi_atama(sayi):
     basamak = 0
-    while (sayi > 0):
+    while sayi > 0:
         basamak += 1
         sayi //= 10
-    if (basamak == 2):
-        print(okunus(sayi))
+    if basamak == 2:
+        print(okunus())
     else:
-        print("İki basamaklı sayı giriniz.")
+        print("İki basamaklı sayı girin!")
 
 
 sayi = int(input("Sayı:"))
-sayiAtama(sayi)
+sayi_atama(sayi)
